7136147f91d49f8d99d6fc7d1533cacd7ac62cb9
Mátyás Fodor <matyas.fodor@gmail.com>
2017-12-03 12:23:09 +0100

    finish day 1

--- d1a/main.py
+++ d1a/main.py
@@ -1,19 +1,6 @@
 # Yields the matching charactersof the input
 # circular
-import collections
-from itertools import takewhile, islice, tee, groupby
-
-
-# Source https://docs.python.org/3/library/itertools.html#itertools-recipes
-def consume(iterator, n):
-    "Advance the iterator n-steps ahead. If n is none, consume entirely."
-    return islice(iterator, n, None)
-
-
-def equals(a):
-    def inner(b):
-        return a == b
-    return inner
+from itertools import groupby
 
 
 def round_iterator(iterator):
@@ -22,59 +9,6 @@ def round_iterator(iterator):
     for i in iterator:
         yield i
     yield first_element
-
-def take_equals():
-    value = None
-    def inner(new_value):
-        nonlocal value
-        if value is None:
-            value = new_value
-        # print('##: ', value, new_value)
-        return value == new_value
-    return inner
-
-def yield_matching_copyfree(string):
-    index = 0
-    while index < len(string):
-        # ret = ''.join(takewhile(take_equals(), iter))
-        ret = ''.join(takewhile(take_equals(), consume(string, index)))
-        index += len(ret)
-        yield ret
-
-
-def yield_matching_iter2(it):
-    value = next(it)
-    while True:
-        counter = 1
-        while True:
-            new_value = next(it)
-            if new_value == value:
-                counter +=1
-            else:
-                # backward compatibility
-                yield [value for _ in range(counter)]
-                value = new_value
-                continue
-    yield [4]
-
-
-def yield_matching_iter(it):
-    while True:
-        it, copy = tee(it)
-        value = list(takewhile(take_equals(), it))
-        consume(it, len(value))
-        if len(value) > 0:
-            yield value
-        else:
-            break
-
-
-def yield_matching(string):
-    return yield_matching_copyfree(string)
-    # while len(string):
-    #     ret = ''.join(takewhile(lambda c: c == string[0], string))
-    #     string = string[len(ret):]
-    #     yield ret
 
 def aggregate(k, g):
     g = list(g)
--- d1a/test_main.py
+++ d1a/test_main.py
@@ -1,31 +1,8 @@
-from d1a.main import yield_matching, round_iterator, main, take_equals, yield_matching_iter2
+from d1a.main import round_iterator, main
 
-
-def test_yield_matching_different_chars():
-    assert list(yield_matching('abcdef')) == list('abcdef')
-
-def test_yeild_duplicates():
-    assert list(yield_matching('aabcde')) == ['aa', 'b', 'c', 'd', 'e']
 
 def test_round_iterator():
     assert list(round_iterator([1, 2, 3, 4].__iter__())) == [1, 2, 3, 4, 1]
-
-# def test_yield_matching_iter():
-#     print('%#%#%#%', list(yield_matching_iter2(iter([1, 1,2,2]))))
-#     assert list(yield_matching_iter2(iter([1, 2, 3, 4]))) == [[1],[2],[3],[4]]
-#     assert list(yield_matching_iter2(iter([1, 1,2,2]))) == [[1,1], [2,2]]
-
-
-def test_take_equals():
-    func = take_equals()
-    func(5)
-    assert func(5)
-    assert not func(6)
-    other_func = take_equals()
-    other_func(11)
-    assert other_func(11)
-    assert func(5)
-    assert not other_func(5)
 
 
 def test_main():
